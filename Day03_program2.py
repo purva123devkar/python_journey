8b083a85cd51d8448ebdba728d3edc8a1c7705b4
Purva Devkar <purvadevkar1607@gmail.com>
2026-08-22 09:01:32 -0700

    Day02_program2.py

---
+++ Day03_program2.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
