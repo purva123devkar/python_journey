772ad635c03cb066d72950734c8d0dba7ec6121a
Purva Devkar <purvadevkar1607@gmail.com>
2026-08-24 09:31:36 -0700

    Add program to find the largest digit in a number

---
+++ Day0_program3.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+largest = 0
+
+while num > 0:
+    digit = num % 10
+
+    if digit > largest:
+        largest = digit
+
+    num = num // 10
+
+print("Largest digit =", largest)
